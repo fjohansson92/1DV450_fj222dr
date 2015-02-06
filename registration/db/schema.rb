972dea802a5cc648e1b6d455da8f8c496917d607
Your Name Here <fj222dr@student.lnu.se>
2015-02-06 18:12:55 +0100

    Added index to email

--- registration/db/schema.rb
+++ registration/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150205172328) do
+ActiveRecord::Schema.define(version: 20150206170431) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email"
@@ -21,5 +21,7 @@ ActiveRecord::Schema.define(version: 20150205172328) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
