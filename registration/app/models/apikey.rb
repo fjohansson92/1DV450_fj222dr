a26e4219281eaebb2aa26f9d6e136cb3e76d7a3c
Your Name Here <fj222dr@student.lnu.se>
2015-02-10 10:41:48 +0100

    Added generation of apikeys

--- registration/app/models/apikey.rb
+++ registration/app/models/apikey.rb
@@ -2,8 +2,18 @@ require 'uri'
 
 class Apikey < ActiveRecord::Base
 	belongs_to :user
+	after_initialize :create_apikey
 
   validates :key, presence:true, length: { maximum: 255 }
 	validates :domain, presence:true, length: { maximum: 255 }, format: { with: URI.regexp }
 
+	private
+		def create_apikey
+			key = nil
+			loop do
+				key = SecureRandom.base64
+				break unless Apikey.exists?(key: key)
+			end
+			self.key = key
+		end
 end
--- registration/test/models/apikey_test.rb
+++ registration/test/models/apikey_test.rb
@@ -5,7 +5,7 @@ class ApikeyTest < ActiveSupport::TestCase
 	def setup
 		user = User.new(email: "a@b.c",name: "Foo Bar", password: "Password", password_confirmation: "Password")
 		user.save
-		@apikey = user.apikeys.create(key: "temprandomstring", domain: "http://localhost:3000")
+		@apikey = user.apikeys.create(domain: "http://localhost:3000")
 	end
 
 	test "should be valid apikey" do
@@ -17,22 +17,6 @@ class ApikeyTest < ActiveSupport::TestCase
 	test "revoked should be false default" do
 		assert_not @apikey.revoked
 	end
-
-
-
-	# Apikey key tests
-  
-  test "key should be required" do
-  	@apikey.key = "  "
-  	assert_not @apikey.valid?
-  end
-
-
-  test "key should not be to long" do
-  	@apikey.key = "a" * 256
-  	assert_not @apikey.valid?
-  end
-
 
 
 	# Apikey domain tests
