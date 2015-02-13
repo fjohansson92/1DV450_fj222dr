2dd3fcd2fb2323600b644475b777627fac4ce8a1
Your Name Here <fj222dr@student.lnu.se>
2015-02-13 15:21:57 +0100

    Added domain validation

--- registration/app/models/domain.rb
+++ registration/app/models/domain.rb
@@ -1,6 +1,6 @@
 require 'uri'
 class Domain < ActiveRecord::Base
+	belongs_to :apikey
 
-
-	#validates :domain, presence:true, length: { maximum: 255 }, format: { with: URI.regexp }, uniqueness: { case_sensitive: false }
+	validates :domain, presence:true, length: { maximum: 255 }, format: { with: URI.regexp }, uniqueness: { case_sensitive: false }
 end
