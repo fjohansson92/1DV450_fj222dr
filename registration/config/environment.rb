46ae108a71605bbb96763d4f73f9e10c68d18b6f
Your Name Here <fj222dr@student.lnu.se>
2015-03-05 10:59:09 +0100

    Set omniauth test mode to false

--- registration/config/environment.rb
+++ registration/config/environment.rb
@@ -4,7 +4,7 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Rails.application.initialize!
 
-OmniAuth.config.test_mode = true
+#OmniAuth.config.test_mode = true
 OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new({
   :provider => 'github',
   :uid => '123545',
