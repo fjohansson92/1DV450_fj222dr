87034a0edda12019eb6099e1b44d2ecb932979ff
Your Name Here <fj222dr@student.lnu.se>
2015-03-05 10:52:37 +0100

    Added login tester

--- registration/app/controllers/api/V1/sessions_controller.rb
+++ registration/app/controllers/api/V1/sessions_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::SessionsController < Api::V1::ApplicationController
 	before_action :set_session, only: :new
 	before_action :get_and_reset_session, only: :create
 	before_action :logged_in_user, only: :destroy 
-	skip_before_filter :authenticate, :only => [:create]
+	skip_before_filter :authenticate, :only => [:create, :new]
 
 	def new 
 		redirect_to "/v1/auth/github"
